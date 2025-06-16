ba934a43d07707b63ba505bc94045a67a577340a
Savely Karmatsky <s.karmatsky@gmail.com>
2025-06-16 19:57:47 +0500

    feat(ui): add underline animation for links

--- app/header.tsx
+++ app/header.tsx
@@ -7,19 +7,44 @@ export default function Header() {
       <nav className="mx-auto flex max-w-screen items-center justify-between p-6 lg:px-8">
         <div className="flex items-center justify-center lg:flex-1">
           <a href="" className="m-1.5 m pr-8 text-xl">
-            О нас
+            <span
+              className="bg-gradient-to-r from-current to-current bg-no-repeat bg-[length:0_1px] 
+              bg-left-bottom transition-[background-size] duration-200 hover:bg-[length:100%_2px]"
+            >
+              О нас
+            </span>
           </a>
           <a href="" className="m-1.5 pr-8 text-xl">
-            Наши проекты
+            <span
+              className="bg-gradient-to-r from-current to-current bg-no-repeat bg-[length:0_1px] 
+              bg-left-bottom transition-[background-size] duration-250 hover:bg-[length:100%_2px]"
+            >
+              Наши проекты
+            </span>
           </a>
           <a href="" className="m-1.5 pr-8 text-xl">
-            Партнерам
+            <span
+              className="bg-gradient-to-r from-current to-current bg-no-repeat bg-[length:0_1px] 
+              bg-left-bottom transition-[background-size] duration-250 hover:bg-[length:100%_2px]"
+            >
+              Партнерам
+            </span>
           </a>
           <a href="" className="m-1.5 pr-8 text-xl">
-            Вопросы
+            <span
+              className="bg-gradient-to-r from-current to-current bg-no-repeat bg-[length:0_1px] 
+              bg-left-bottom transition-[background-size] duration-250 hover:bg-[length:100%_2px]"
+            >
+              Вопросы
+            </span>
           </a>
           <a href="" className="m-1.5 pr-8 text-xl">
-            Контакты
+            <span
+              className="bg-gradient-to-r from-current to-current bg-no-repeat bg-[length:0_1px] 
+              bg-left-bottom transition-[background-size] duration-250 hover:bg-[length:100%_2px]"
+            >
+              Контакты
+            </span>
           </a>
           <a href="/" className="m-1">
             <Image
